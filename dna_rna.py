d411c202b91a1dd7bc496957fde4515ea9098e3b
João Ricardo <32642843+joaoricardao@users.noreply.github.com>
2019-03-27 19:55:31 -0300

    Update dna_rna.py

--- dna_rna.py
+++ dna_rna.py
@@ -1,3 +1,13 @@
+#Sequencia de DNA
 dna_seq="TGGGAAAAACCCGTCTTACGGG"
 rna_seq=dna_seq.replace('T','U')
 print (rna_seq)
+
+#Qual o tamanho da sequencia de DNA
+print ("Tamanho da cadeia de DNA: ", len(dna_seq))
+
+#Quantos nucleotídeos são do tipo "adenina"
+print ("Nucleotídeos do tipo Adenina: ", dna_seq.count("A"))
+print ("Nucleotídeos do tipo Timina: ", dna_seq.count("T"))
+print ("Nucleotídeos do tipo Citosina: ", dna_seq.count("C"))
+print ("Nucleotídeos do tipo Guanina: ", dna_seq.count("G"))
